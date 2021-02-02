464efdd5dc1d4648604153b2d0fddb458d83c1f4
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-01 23:27:22 -0500

    Update Q3_ArrayInsertionDemo.java

--- lab1/Q3_ArrayInsertionDemo/Q3_ArrayInsertionDemo.java
+++ lab1/Q3_ArrayInsertionDemo/Q3_ArrayInsertionDemo.java
@@ -2,3 +2,6 @@ public class Q3_ArrayInsertionDemo{
 
 	public static int[] insertIntoArray(int[] beforeArray,int indexToInsert,int valueToInsert){
 		int newArr[]= new int[1+beforeArray.length];
+		for(int i=0;i<indexToInsert;i++){
+			newArr[i]=beforeArray[i];
+		}
