12e32b276898513ccd0a5e3d32ed540db29f5ee9
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-03 22:51:32 -0500

    Update Q3_SquareArray.java

--- lab1/Q3_SquareArray.java
+++ lab1/Q3_SquareArray.java
@@ -1,3 +1,8 @@
 public class Q3_SquareArray{
 
 	public static int[] createArray(int size) {
+		int[] anArray;
+		anArray = new int[size];
+		for(int i = 0; i < anArray.length; i++){
+			anArray[i] = i * i;
+		}
