67e9a2ce067dfd3135f4fbb6bec7ce0bf5fac6b4
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-07 21:48:57 -0500

    Update Q3_ReverseSortDemo.java

--- lab1/Q3_ReverseSortDemo/Q3_ReverseSortDemo.java
+++ lab1/Q3_ReverseSortDemo/Q3_ReverseSortDemo.java
@@ -4,3 +4,5 @@ public class Q3_ReverseSortDemo {
 		unorderedLetters = new char[]{'b', 'm', 'z', 'a', 'u'};
 		reverseSort(unorderedLetters);
 		for (int i = 0 ; i < unorderedLetters.length; i++ )
+			System.out.println(unorderedLetters[i]);
+	}
