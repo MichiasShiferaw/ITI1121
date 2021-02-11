993360e9450254a91987dde22bfe3566101316ad
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-10 22:18:03 -0500

    Update Q5.java

--- lab1/Q5/Q5.java
+++ lab1/Q5/Q5.java
@@ -1,1 +1,10 @@
 public class Q5{
+
+  	public static void main(String[] args){
+		boolean[] test = new boolean[8];
+
+		//test for isPrime
+		test[0]  = (isPrime(0) == false);
+		test[1] = (isPrime(2) == true);
+		test[2] = (isPrime(7) == true);
+		test[3] = (isPrime(15) == false);
