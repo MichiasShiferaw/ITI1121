7d2e0622d2bf5b6c9c166f124eacd4411cad9761
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-13 23:33:25 -0500

    Update Q6.java

--- lab1/Q6/Q6.java
+++ lab1/Q6/Q6.java
@@ -4,3 +4,6 @@ public class Q6{
 	public static void main(String[] args){
 		double[]notes=new double[10];
 		System.out.print("Enter your grades (on this line): ");
+		Scanner scanName = new Scanner(System.in);
+		for(int i=0; i< 10; i++){
+			notes[i]=scanName.nextDouble();
