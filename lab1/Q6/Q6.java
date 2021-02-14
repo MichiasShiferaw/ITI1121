aafcce73487fe2c4ce48da863c4159c90a90ad85
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-13 23:33:37 -0500

    Update Q6.java

--- lab1/Q6/Q6.java
+++ lab1/Q6/Q6.java
@@ -7,3 +7,8 @@ public class Q6{
 		Scanner scanName = new Scanner(System.in);
 		for(int i=0; i< 10; i++){
 			notes[i]=scanName.nextDouble();
+		}
+		double avg_val=calculateAverage(notes);
+		double mid=calculateMedian(notes);
+		int pass=calculateNumberPassed(notes);
+		int failed=calculateNumberFailed(notes);
