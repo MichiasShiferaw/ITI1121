45c84c8a1401eb60165d51fdcd71c16103a0eba2
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-17 22:29:07 -0500

    Create Combination.java

---
+++ lab2/Combination.java
@@ -0,0 +1,7 @@
+public class Combination {
+
+    // Instance variables.
+    // Remove this comment and declare your instance variables here
+    private int first;
+    private int second;
+    private int third;
