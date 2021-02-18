d82c47785e6f433df0c648a7f9f02ee850ba821b
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-17 22:29:52 -0500

    Update Combination.java

--- lab2/Combination.java
+++ lab2/Combination.java
@@ -11,3 +11,10 @@ public class Combination {
     // note: we have to use "this" in order to distinguish
     // between the parameter first and the instance
     // variable first (similarly for second and third).
+
+    public Combination( int first, int second, int third ) {
+        // Your code here
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
