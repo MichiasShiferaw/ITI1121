b774cdaf3f28b16dc78be00f4c940e15590dd4b4
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-27 21:48:56 -0500

    Create Utils.java

---
+++ lab3/Utils.java
@@ -0,0 +1,13 @@
+public class Utils {
+
+    /**
+     * Returns a copy of the array in where each word occurring in the array
+     * 'what' has been replaced by the word occurring in the same position
+     * in the array 'with'.
+     *
+     * @param in an array of Strings;
+     * @param what an array of words to be replaced;
+     * @param with an array of replacement words;
+     * @return a new array idententical to 'in' except that all the occurrences of words
+     * found in 'what' have been replaced by the corresponding word from 'with'.
+     */
