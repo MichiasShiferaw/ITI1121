1ff18f9164a2410ea4486f945431589ee5faaf5b
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-02-28 22:30:20 -0500

    Update Utils.java

--- lab3/Utils.java
+++ lab3/Utils.java
@@ -3,7 +3,7 @@ public class Utils {
     /**
      * Returns a copy of the array in where each word occurring in the array
      * 'what' has been replaced by the word occurring in the same position
-     * in the array 'with'.
+     * in the array 'with'
      *
      * @param in an array of Strings;
      * @param what an array of words to be replaced;
