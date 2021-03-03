b27c27e7584430a84add384f2cdf54c3c4cef96b
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-03-02 22:03:59 -0500

    Update DoorLock.java

--- lab2/DoorLock.java
+++ lab2/DoorLock.java
@@ -2,3 +2,9 @@ public class DoorLock {
 
       // Constant.
     public static final int MAX_NUMBER_OF_ATTEMPTS = 3;
+          // Instance variables.
+    private Combination combination;
+    private boolean open;
+    private boolean activated;
+    private int numberOfAttempts;
+
