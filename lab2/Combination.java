8de5446157c3df1237a179f39402151ff3590e1a
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-03-04 21:26:18 -0500

    Update Combination.java

--- lab2/Combination.java
+++ lab2/Combination.java
@@ -37,3 +37,4 @@ public class Combination {
     // Returns a String representation of this Combination.
    public String toString() {
         // Put your code here and remove the line below
+        return first + ":" + second + ":" + third;
