7c187ef1c48f761824fdf8d0739a6e129467dc65
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-03-05 19:28:11 -0500

    Update Rational.java

--- lab3/Rational.java
+++ lab3/Rational.java
@@ -7,3 +7,8 @@ public class Rational {
     public Rational(int numerator) {
 	this(numerator, 1);
     }
+	
+    public Rational(int numerator, int denominator) {
+	if (denominator < 0) {
+	    denominator = -1 * denominator;
+	    numerator = -1 * numerator;
