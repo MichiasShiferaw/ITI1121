68900d4e62686c61c33957bcda4550a089112b17
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-03-17 22:45:51 -0400

    Update Post.java

--- lab4/Post.java
+++ lab4/Post.java
@@ -2,3 +2,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Post implements Likeable, Comparable<Post> {
+
+    protected int likes;
+    private Date timeStamp;
+    private String userName;
