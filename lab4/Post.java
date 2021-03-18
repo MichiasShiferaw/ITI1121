fcfa2159dc12e762e1fba95a96ebb1c650c671d9
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-03-17 22:45:59 -0400

    Update Post.java

--- lab4/Post.java
+++ lab4/Post.java
@@ -6,3 +6,5 @@ public class Post implements Likeable, Comparable<Post> {
     protected int likes;
     private Date timeStamp;
     private String userName;
+    public Post(String userName) {
+      likes = 0;
