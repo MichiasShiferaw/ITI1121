b9fc072c9331b3ee2c245cb5bb48e7b97a9e3289
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-03-24 21:28:59 -0400

    Update TextPost.java

--- lab4/TextPost.java
+++ lab4/TextPost.java
@@ -1,3 +1,10 @@
 
 public class TextPost extends Post {
     private String message;
+
+    
+    public TextPost(String userName, String message) {
+      // Your code here.
+      super(userName);
+      this.message=message;
+    }
