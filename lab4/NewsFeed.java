e74168137608db90aff4e2215f56ad3da91316e6
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-03-30 21:07:12 -0400

    Update NewsFeed.java

--- lab4/NewsFeed.java
+++ lab4/NewsFeed.java
@@ -4,3 +4,8 @@ public class NewsFeed {
     private Post[] messages;
     private int size;
     public static final int MAX_SIZE = 25;
+    public NewsFeed() {
+    	// Your code here.
+		this.size=0;
+		this.messages = new Post[MAX_SIZE];
+    }
