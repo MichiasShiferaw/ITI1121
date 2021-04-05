9940aa3338cc983230012cba609f13081ed46b12
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-04 20:44:13 -0400

    Update AbstractSeries.java

--- lab5/AbstractSeries.java
+++ lab5/AbstractSeries.java
@@ -4,3 +4,6 @@ public abstract class AbstractSeries implements Series {
     public double[] take(int k) {
         //initializing the array
         double nextSums[] = new double[k];
+        //using next() method, assigning next k numbers to array
+        for (int i = 0; i < k; i++) {
+            nextSums[i] = next();
