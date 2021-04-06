a102593d27a5e96bfcf0fbe4bf242e8606f45bd7
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-05 21:47:21 -0400

    Update Arithmetic.java

--- lab5/Arithmetic.java
+++ lab5/Arithmetic.java
@@ -4,3 +4,5 @@ public class Arithmetic extends AbstractSeries {
     // instance variables
     private double currentSum; //current partial sum
     private int n; //current n
+
+    public double next() {
