baed27158a199190589f3e659d7c59ea2851fffa
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-05 21:47:28 -0400

    Update Arithmetic.java

--- lab5/Arithmetic.java
+++ lab5/Arithmetic.java
@@ -6,3 +6,13 @@ public class Arithmetic extends AbstractSeries {
     private int n; //current n
 
     public double next() {
+        // implement the method
+        
+        //adding n to current sum
+        currentSum+=n;
+        //incrementing n for next call
+        n++;
+        //returning current sum
+        return currentSum;        
+        
+    }
