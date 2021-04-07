14c0c49464ff0d15c29e3fb176376cfc45c57cb2
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-06 22:14:05 -0400

    Update Book.java

--- lab5/Book.java
+++ lab5/Book.java
@@ -5,3 +5,10 @@ public class Book {
 	
     private String author;
     private String title;
+
+    public Book (String author, String title, int year) {
+        // Your code here
+        this.author = author;
+        this.title = title;
+        this.year = year;
+    }
