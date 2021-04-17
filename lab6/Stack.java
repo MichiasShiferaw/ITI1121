e385d0a5a4de6debbe92e2eefdb8ec2c869868d2
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-16 22:32:15 -0400

    Update Stack.java

--- lab6/Stack.java
+++ lab6/Stack.java
@@ -1,1 +1,12 @@
 
+    public abstract void push( E element);
+
+    /**
+     * Removes all the elements from this stack.  Immediately after a
+     * call to the method clear() the method isEmpty() will return
+     * true.
+     */
+
+    public abstract void clear();
+
+}
