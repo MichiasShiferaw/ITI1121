c3b21e4cbcf3f605729a7a2fbd89c653278b0c2f
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-16 22:32:25 -0400

    Update Stack.java

--- lab6/Stack.java
+++ lab6/Stack.java
@@ -1,3 +1,18 @@
+    public abstract E peek();
+
+    /**
+     * Removes and returns the element at the top of this stack.
+     *
+     * @return The top element of this stack.
+     */
+
+    public abstract E pop();
+
+    /**
+     * Puts an element onto the top of this stack.
+     *
+     * @param element the element be put onto the top of this stack.
+     */
 
     public abstract void push( E element);
 
