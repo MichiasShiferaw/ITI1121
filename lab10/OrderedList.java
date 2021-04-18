6e520ad43cb6afb9d5d6c0ceeac9326d9fa25b2b
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-17 23:32:09 -0400

    Update OrderedList.java

--- lab10/OrderedList.java
+++ lab10/OrderedList.java
@@ -1,1 +1,18 @@
+import java.util.NoSuchElementException;
 
+public class OrderedList implements OrderedStructure {
+
+    // Implementation of the doubly linked nodes (nested-class)
+
+    private static class Node {
+
+      	private Comparable value;
+      	private Node previous;
+      	private Node next;
+
+      	private Node ( Comparable value, Node previous, Node next ) {
+      	    this.value = value;
+      	    this.previous = previous;
+      	    this.next = next;
+      	}
+    }
