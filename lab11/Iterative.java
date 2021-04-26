d4c216828b8803254d4ca6b08dd63c1a8b0e4859
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-25 21:56:28 -0400

    Update Iterative.java

--- lab11/Iterative.java
+++ lab11/Iterative.java
@@ -3,3 +3,6 @@ public class Iterative {
     public static BitList complement( BitList in ) {
         BitList result;
         result = new BitList();
+
+                Iterator i = in.iterator();
+        Iterator j = result.iterator();
