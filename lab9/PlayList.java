44bd2ef8a0217fc74b7dca729d9a5ddff79d8b31
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-04-26 21:50:33 -0400

    Update PlayList.java

--- lab9/PlayList.java
+++ lab9/PlayList.java
@@ -9,3 +9,18 @@ public class PlayList {
     private Song[] songs;
     private int count;
     private int capacityIncrement;
+
+    
+       // Constructor
+    // See below for a new usage of the reserved keyword this.
+
+    public PlayList( int initialCapacity, int capacityIncrement ) {
+    
+        if ( initialCapacity < 1 )
+            throw new IllegalArgumentException( "Illegal capacity: "+ initialCapacity );
+    
+        if ( capacityIncrement < 1 )
+            throw new IllegalArgumentException( "Illegal increment: "+ capacityIncrement );
+    
+        songs = new Song[ initialCapacity ];
+        this.capacityIncrement = capacityIncrement;
